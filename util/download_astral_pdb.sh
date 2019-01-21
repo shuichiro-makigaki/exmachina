0cb2fe12ebd776a93328e48c2c467bb267716dfc
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2019-01-21 14:07:01 +0900

    Update

--- util/download_astral_pdb.sh
+++ util/download_astral_pdb.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -x
 
-cd data
+if [[ ! -e data/train ]]; then
+  mkdir data/train
+fi
+cd data/train
+
 wget http://scop.berkeley.edu/downloads/pdbstyle/pdbstyle-sel-gs-bib-40-1.75.tgz
+tar zxf pdbstyle-sel-gs-bib-40-1.75.tgz && rm pdbstyle-sel-gs-bib-40-1.75.tgz
